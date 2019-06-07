8ef0bb7dcf53c609fed6ec9a0e189097938cb7a9
chmd2019 <51176339+chmd2019@users.noreply.github.com>
2019-06-07 17:49:22 -0500

    Update Config.php

--- icloud/Model/Config.php
+++ icloud/Model/Config.php
@@ -1,8 +1,8 @@
 <?php
 
-$client_id = '144850677714-m6026buduis4id443pjcvmk31ceu8e3i.apps.googleusercontent.com';
+$client_id = '144850677714-m6026buduis4id443pjcvmk31ceu8e3i.apps.googleusercontent.com/';
 $client_secret ='CsqLEfWCKZRH7egeKkXhZyn6';
-$redirect_uri = 'https://www.chmd.edu.mx/pruebas/icloud';
+$redirect_uri = 'https://www.chmd.edu.mx/pruebas/icloud/';
 
 $client = new Google_Client();
 $client->setClientId($client_id);
